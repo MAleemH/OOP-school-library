7e817278c714914fd16a0a49bdcf9dd296dfc2d9
Your Name <you@example.com>
2023-04-14 15:52:26 +0500

    Created Rental class

---
+++ rental.rb
@@ -0,0 +1,12 @@
+class Rental
+  attr_accessor :book, :person, :date
+
+  def initialize(book, person, date)
+    @book = book
+    @person = person
+    @date = date
+
+    book.add_rental(self)
+    person.add_rental(self)
+  end
+end
