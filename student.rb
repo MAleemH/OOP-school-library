8eb3e0fa4f76c077165e710c6669d2591b4b435b
Your Name <you@example.com>
2023-04-14 15:52:59 +0500

    Added classroom setter

--- student.rb
+++ student.rb
@@ -1,11 +1,17 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :name, :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+    @name = name
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
