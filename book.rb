0787e05530c787cd4d8e64e5b35dfb9988dbd269
Your Name <you@example.com>
2023-04-14 15:47:40 +0500

    Created Book class and added rental method

---
+++ book.rb
@@ -0,0 +1,12 @@
+class Book
+    attr_accessor :title, :author, :rentals
+    def initialize(title, author)
+        @title = title
+        @author = author
+        @rentals = []
+    end
+
+    def add_rental(book, date)
+        @rentals << Rental.new(book, self, date)
+    end
+end
