8ba33202ca45f7dc5c24f04402566190fc8e28c1
MAleemH <aleemhashmi4321@gmail.com>
2023-04-20 12:50:21 +0500

    Added methods to store and load data

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'json'
 require './book'
 require './classroom'
 require './decorator'
@@ -76,5 +77,47 @@ class App
     @rentals.each do |rental|
       puts "Date: #{rental.date}, Book: \"#{rental.book.title} by #{rental.book.author}" if rental.person.id == id
     end
+  end
+
+  # store data
+  def store_data
+    File.write('./data/people.json', JSON.generate(@people.map(&:to_h)))
+    File.write('./data/books.json', JSON.generate(@books.map(&:to_h)))
+    File.write('./data/rentals.json', JSON.generate(@rentals.map(&:to_h)))
+  end
+
+  # load data
+  def load_data
+    if File.exist?('./data/people.json')
+      @people = JSON.parse(File.read('./data/people.json')).map do |person_hash|
+        case person_hash['type']
+        when 'Student'
+          Student.new(person_hash['age'], person_hash['id'], person_hash['name'], parent_permission: person_hash['parent_permission'])
+        when 'Teacher'
+          Teacher.new(person_hash['age'], person_hash['specialization'], person_hash['name'], person_hash['id'])
+        end
+      end
+    end
+
+    if File.exist?('./data/books.json')
+      @books = JSON.parse(File.read('./data/books.json')).map do |book_hash|
+        Book.new(book_hash['title'], book_hash['author'])
+      end
+    end
+
+    if File.exist?('./data/rentals.json')
+      @rentals = JSON.parse(File.read('./data/rentals.json')).map do |rental_hash|
+        Rental.new(
+          rental_hash['data'],
+          rental_hash['person'],
+          rental_hash['book']
+        )
+      end
+    end
+  end
+
+  # exit handler
+  def exit_handler
+    store_data
   end
 end
