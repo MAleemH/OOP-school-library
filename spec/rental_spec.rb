0b71fc2602da88290ea617af9cbde8c17c10c6d2
mrprotocoll <lekanvgbg@gmail.com>
2023-04-21 12:04:22 +0100

    Fixed errors and Linters

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -2,26 +2,28 @@ require 'spec_helper'
 require 'date'
 
 describe Rental do
-    book = Book.new "Title", "Author"
-    person = Student.new 23, 'Full-stack' "Muhammad Asif"
-    before :each do
-        @rental = Rental.new book, person, Date.today
-    end
-    context "#new" do
-        it "takes three parameters and return a student object" do
-            expect(@rental).to be_an_instance_of Rental
-        end
-    end
+  book = Book.new 'Title', 'Author'
+  student = Student.new 23, 'Full-stack', 'Muhammad Asif'
 
-    context "#rental book" do
-        it "rental book must return a book object" do
-            expect(@rental.book).to be_an_instance_of Book
-        end
-    end
+  before :each do
+    @rental = Rental.new book, student, Date.today
+  end
 
-    context "#rental person" do
-        it "rental person must be a person object" do
-            expect(@rental.person).to be_an_instance_of Person
-        end
+  context '#new' do
+    it 'takes three parameters and return a student object' do
+      expect(@rental).to be_an_instance_of Rental
     end
+  end
+
+  context 'rental book' do
+    it 'rental book must return a book object' do
+      expect(@rental.book).to be_an_instance_of Book
+    end
+  end
+
+  context 'rental student' do
+    it 'rental person must be a student or teacher object' do
+      expect(@rental.person).to be_an_instance_of(Student).or be_an_instance_of(Teacher)
+    end
+  end
 end
